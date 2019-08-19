2377265de75a0e51b10561d21452dc90cabacac4
obedmax00 <sou@liberty.edu>
2019-08-19 16:26:35 -0400

    add testserviceAll

---
+++ src/test/java/com/ascending/estate/service/TestServiceAll.java
@@ -0,0 +1,15 @@
+package com.ascending.estate.service;
+
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AgentServiceTest.class,
+        CustomerServiceTest.class,
+        HouseServiceTest.class
+})
+public class TestServiceAll {
+}
